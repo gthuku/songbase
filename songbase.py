c7e683df49c6e50f9a0bd32cb20f611e28eca382
George Thuku <g.thuku15@live.com>
2017-11-14 13:41:51 -0500

    added web forms

--- songbase.py
+++ songbase.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'fkuywegffkufbf8ywerwp'
 
 @app.route('/')
 def index():
-    # return '<h1> hello world!!!!</h1>'
     return render_template('index.html')
+
+
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    # how to get form data is different for GET vs. POST
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            first_name = session.get('first_name')
+        return render_template('form-demo.html', first_name=first_name)
+    if request.method == 'POST':
+        session['first_name'] = request.form['first_name']
+        return redirect(url_for('form_demo'))
 
 
 @app.route('/user/<string:name>/')
 def get_user(name):
-    # return 'hello %s your age is %d' % (name, 3)
     return render_template('user.html', user_name=name)
 
 
